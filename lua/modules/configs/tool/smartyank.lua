cd4bfc33d1e79c9dbe31aa036b68c7a24db894ca
Yuchen Fang <arthurfyc@gmail.com>
2023-03-02 15:30:14 +0800

    minor

--- lua/modules/configs/tool/smartyank.lua
+++ lua/modules/configs/tool/smartyank.lua
@@ -17,7 +17,7 @@ return function()
 			enabled = true,
 			escseq = "tmux", -- use tmux escape sequence, only enable if you're using remote tmux and have issues (see #4)
 			ssh_only = false, -- false to OSC52 yank also in local sessions
-			silent = false, -- true to disable the "n chars copied" echo
+			silent = true, -- true to disable the "n chars copied" echo
 			echo_hl = "Directory", -- highlight group of the OSC52 echo message
 		},
 	})
