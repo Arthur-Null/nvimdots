f1860e3622418d7970fa90a38b7111d67a3c7a82
Yuchen Fang <arthurfyc@gmail.com>
2023-03-09 17:36:48 +0800

    add pydocstring

--- lua/modules/plugins/custom.lua
+++ lua/modules/plugins/custom.lua
@@ -1,15 +1,21 @@
 local custom = {}
 
 custom["rose-pine/neovim"] = {
-    name = 'rose-pine',
-    lazy = false,
-    priority = 1000,
-    config = require("custom.rose-pine"),
+	name = "rose-pine",
+	lazy = false,
+	priority = 1000,
+	config = require("custom.rose-pine"),
 }
 
 custom["kylechui/nvim-surround"] = {
-    lazy = false,
-    config = require("custom.nvim-surround"),
+	lazy = false,
+	config = require("custom.nvim-surround"),
+}
+
+custom["heavenshell/vim-pydocstring"] = {
+	lazy = true,
+	cmd = { "Pydocstring" },
+	build = { "make install" },
 }
 
 return custom
