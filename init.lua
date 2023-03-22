89f3a0f339462378c76537ff717c60d9c6ff0ec1
Yuchen Fang <arthurfyc@gmail.com>
2023-03-22 17:53:55 +0800

    🎉 feat(init.lua): add neovide configuration and keymaps 🚀 feat(pyright.lua): add pyright language server configuration
    The first change adds configuration and keymaps for the neovide plugin. The configuration includes setting the font, enabling fullscreen mode, and setting the input mode for macOS. The keymaps include shortcuts for saving, copying, and pasting in normal, visual, and command modes.
    
    The second change adds configuration for the pyright language server. The configuration includes the command to start the server, the filetypes to use it with, and the root directory pattern to search for configuration files. The configuration also includes settings for the python analysis, such as auto search paths, use of library code for types, and diagnostic mode. Additionally, a command is added to organize imports.

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
 if not vim.g.vscode then
 	require("core")
 end
+if vim.g.neovide then
+	vim.o.guifont = "FiraCode Nerd Font Mono:h14" -- text below applies for VimScript
+	vim.g.neovide_input_macos_alt_is_meta = true
+	vim.g.neovide_fullscreen = true
+	vim.g.neovide_input_use_logo = 1 -- enable use of the logo (cmd) key
+	vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert modevim.g.neovide_remember_window_size = true
+end
