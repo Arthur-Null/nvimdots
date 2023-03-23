8bba44624953a2b1f6d97e5447d15562bcd8c015
Yuchen Fang <arthurfyc@gmail.com>
2023-03-23 16:10:40 +0800

    🎨 style(init.lua): refactor key mappings and add quit confirmation This commit refactors the key mappings to use the `nvim_set_keymap` function instead of `keymap.set`. It also adds a confirmation prompt when quitting neovim. The new key mappings are more concise and easier to read.

--- init.lua
+++ init.lua
@@ -6,10 +6,9 @@ if vim.g.neovide then
 	vim.g.neovide_input_macos_alt_is_meta = true
 	vim.g.neovide_fullscreen = true
 	vim.g.neovide_input_use_logo = 1 -- enable use of the logo (cmd) key
-	vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
-	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert modevim.g.neovide_remember_window_size = true
+	vim.g.neovide_confirm_quit = true
+	vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("t", "<D-v>", "<C-\\><C-n>+p<CR>i", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 end
